94a2ac625f6af6245846ad1b09a56dab0cd8e65c
shreyash Yadav <shreyashy49@gmail.com>
2026-03-06 02:36:53 +0530

    complete assignment 01

---
+++ assignment/01/app.js
@@ -0,0 +1,53 @@
+//ques-1
+// let str =prompt("enter string to check the lenght");
+// let x = str.length
+// console.log(x);
+
+
+// //ques-4
+
+// let a= 10;
+// let b=5;
+
+
+// if(a>b){
+//     console.log("greater")
+// }
+// else if(a<b){
+//     console.log("smaller");
+// }
+// else{
+//     console.log("equal");
+// }
+
+//ques-5
+let first = "shreyash"
+let last = "yadav"
+
+console.log(first+" " +last);
+
+
+//section-2 function
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(2,3))
+
+
+//ques-2
+
+const square =(a)=>{
+    return a*a;
+}
+
+console.log(square(5))
+
+
+
+//convert function into arrow function
+
+const multiply =(a,b)=>{
+    return a*b;
+}
